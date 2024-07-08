9812423f9a59bf3485f92188675620fa1a99c7b4
Leandro <Leandro@leandro.com>
2024-07-08 09:22:54 -0300

    Created controller fetch resume questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthenticateController } from './controllers/authenticate.controller';
 import { envSchema } from './env';
 import { AuthModule } from './auth/auth.module'
 import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { CreateQuestionController } from './controllers/create-question.controll
     }),
     AuthModule
   ],
-  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
+  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController,FetchRecentQuestionsController],
   providers: [PrismaService],
 })
 export class AppModule {}
